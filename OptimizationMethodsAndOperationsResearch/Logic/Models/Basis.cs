1bac8972e880d27a5abe56cacdb8a1c78f3f952f
kallDun <igorxlonline24@gmail.com>
2022-03-23 22:01:00 +0200

    Add project files.

---
+++ OptimizationMethodsAndOperationsResearch/Logic/Models/Basis.cs
@@ -0,0 +1,20 @@
+﻿using Fractions;
+using System;
+
+namespace OptimizationMethodsAndOperationsResearch.Logic.Models
+{
+    public class Basis : ICloneable
+    {
+        public Basis(int index, Fraction value, bool isHugeNumber = false)
+        {
+            Index = index;
+            Value = value;
+            IsHugeNumber = isHugeNumber;
+        }
+        public int Index { get; set; }
+        public Fraction Value { get; set; }
+        public bool IsHugeNumber { get; set; }
+
+        public object Clone() => new Basis(Index, Value, IsHugeNumber);
+    }
+}
---
+++ OptimizationMethodsAndOperationsResearch/Logic/Models/Table.cs
@@ -0,0 +1,33 @@
+﻿using Fractions;
+using System;
+using System.Linq;
+
+namespace OptimizationMethodsAndOperationsResearch.Logic.Models
+{
+    public class Table : ICloneable
+    {
+        public Table(int columns, int rows, Fraction[][] matrix, Basis[] columnBasises, Basis[] rowsBasises)
+        {
+            Columns = columns;
+            Rows = rows;
+            Matrix = matrix;
+            ColumnBasises = columnBasises;
+            RowsBasises = rowsBasises;
+        }
+        public Fraction this[int i, int j]
+        {
+            get => Matrix[i][j];
+            set => Matrix[i][j] = value;
+        }
+        public int Columns { get; private set; }
+        public int Rows { get; private set; }
+        public Fraction[][] Matrix { get; private set; }
+        public Basis[] ColumnBasises { get; private set; }
+        public Basis[] RowsBasises { get; private set; }
+
+        public object Clone() => new Table(Columns, Rows,
+            Matrix.Select(x => x.Clone() as Fraction[]).ToArray(),
+            ColumnBasises.Select(x => x.Clone() as Basis).ToArray(),
+            RowsBasises.Select(x => x.Clone() as Basis).ToArray());
+    }
+}
