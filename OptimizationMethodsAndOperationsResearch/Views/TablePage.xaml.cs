703db48d51078e2e231d8cfc6e8357b4007e58c4
pc_127_1@outlook.com <pc_127_1@outlook.com>
2022-03-24 15:35:48 +0200

    clear usings

--- OptimizationMethodsAndOperationsResearch/Views/TablePage.xaml.cs
+++ OptimizationMethodsAndOperationsResearch/Views/TablePage.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using OptimizationMethodsAndOperationsResearch.Logic.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OptimizationMethodsAndOperationsResearch.Views
 {
