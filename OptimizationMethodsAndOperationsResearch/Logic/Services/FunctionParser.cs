994321275bf9cdd81a4046492c41985914e77be0
Savienkov Maksym <froggt25@gmail.com>
2022-03-31 00:53:53 +0300

    implemented methods GetNextTable, IsOptimizated & GetResults (needs verification)

--- OptimizationMethodsAndOperationsResearch/Logic/Services/FunctionParser.cs
+++ OptimizationMethodsAndOperationsResearch/Logic/Services/FunctionParser.cs
@@ -11,6 +11,18 @@ namespace OptimizationMethodsAndOperationsResearch.Logic.Services
     {
         public Table ToTable(string function, string equations)
         {
+            string[] condition = function.Trim().Split(new char[] { '>' });
+
+            string minormax = condition[1].Trim();
+            string expression = condition[0].Substring(0, condition[0].Length - 1).Trim();
+
+            string[] leftRightOfExp = expression.Split(new char[] { '=' });
+
+            string leftPartOfExp = leftRightOfExp[0].Trim();
+            string rightPartOfExp = leftRightOfExp[1].Trim();
+
+
+
             throw new NotImplementedException();
         }
     }
--- OptimizationMethodsAndOperationsResearch/Logic/Services/SimplexMethodCalculator.cs
+++ OptimizationMethodsAndOperationsResearch/Logic/Services/SimplexMethodCalculator.cs
@@ -2,24 +2,139 @@
 using OptimizationMethodsAndOperationsResearch.Logic.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OptimizationMethodsAndOperationsResearch.Logic.Services
 {
     class SimplexMethodCalculator
     {
+        public bool hasSolution { get; private set; } = true;
+
         public bool IsOptimizated(Table table)
         {
+            if (HasSolutions(table))
+            {
+                return table.Matrix[table.Matrix.Length - 1].Skip(1).Where(x => x <= 0).Count() == 0;
+            }
+            else
+            {
+                hasSolution = false;
+                return true;
+            }
             throw new NotImplementedException();
+        }
+
+        private bool HasSolutions(Table table)
+        {
+            for (int i = 0; i < table.Matrix.GetLength(1); i++)
+            {
+                if (table.Matrix[table.Matrix.Length - 1].Skip(1).ToArray()[i] > 0 &&
+                    table.Matrix
+                    .Take(table.Matrix.Length - 1)
+                    .Where(x => x[i] > 0)
+                    .Count() == 0)
+                {
+                    return false;
+                }
+            }
+            for (int i = 0; i < table.ColumnBasises.Length; i++)
+            {
+                if (table.ColumnBasises[i].IsHugeNumber && table.Matrix[0][i].IsNegative)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public Table GetNextTable(Table table)
         {
-            throw new NotImplementedException();
+            int indexOfInputingVector = GetIndexOfInputingVector(table);
+            int indexOfDeletingVector = GetIndexOfDeletingVector(table, indexOfInputingVector);
+            Fraction keyElem = table.Matrix[indexOfDeletingVector][indexOfInputingVector];
+
+            Table newTable = (Table)table.Clone();
+            newTable.ColumnBasises[indexOfDeletingVector] = table.RowBasises[indexOfInputingVector - 1];
+            
+            table.Matrix[indexOfDeletingVector]
+                .Select(x => x = x / keyElem)
+                .ToArray()
+                .CopyTo(newTable.Matrix[indexOfDeletingVector],0);
+
+            for (int i = 0; i < table.Matrix.Length; i++)
+            {
+                if (i != indexOfDeletingVector)
+                {
+                    table.Matrix[i][indexOfInputingVector] = 0;
+                }
+            }
+
+            for (int i = 0; i < table.Matrix.Length; i++)
+            {
+                if (i != indexOfDeletingVector)
+                {
+                    table.Matrix[i]
+                        .Select((x, ind) => x = (x * keyElem - table.Matrix[indexOfDeletingVector][ind] * table.Matrix[i][indexOfInputingVector]) / keyElem)
+                        .ToArray()
+                        .CopyTo(newTable.Matrix[i], 0);
+                }
+            }
+
+            return newTable;
         }
 
         public Dictionary<int, Fraction> GetResults(Table table)
         {
-            throw new NotImplementedException();
+            Dictionary<int, Fraction> results = new Dictionary<int, Fraction>();
+            for (int i = 0; i < table.RowBasises.Length; i++)
+            {
+                if (table.ColumnBasises.Select(elem => elem.Index).ToArray().Contains(i))
+                {
+                    results.Add(i, table.RowBasises[i].Value);
+                }
+                else
+                {
+                    results.Add(i, 0);
+                }
+            }
+            return results;
+        }
+
+        private int GetIndexOfInputingVector(Table table)
+        { 
+            return Array.IndexOf(
+                table.Matrix[table.Matrix.Length - 1],
+                table.Matrix[table.Matrix.Length - 1]
+                    .Where(x => x > 0)
+                    .Select(x => Math.Abs(x.ToDouble()))
+                    .Min());
+        }
+
+        private int GetIndexOfDeletingVector(Table table, int indexOfInputingVector)
+        {
+            int index = -1;
+            Fraction min = Fraction.Zero;
+            for (int i = 0; i < table.Matrix[0].Length; i++)
+            {
+                Fraction fraction = table.Matrix[i][indexOfInputingVector];
+                if (fraction > 0)
+                {
+                    min = table.Matrix[i][0] / fraction;
+                }
+            }
+
+            for (int i = 0; i < table.Matrix[0].Length; i++)
+            {
+                Fraction fraction = table.Matrix[i][indexOfInputingVector];
+                Fraction relation = table.Matrix[i][0] / fraction;
+                if (fraction > 0 && relation < min)
+                {
+                    index = i;
+                    min = relation;
+                }
+            }
+
+            return index;
         }
 
     }
